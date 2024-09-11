687e38e9f6b837a66809a5661228544c44fab6e9
abbie <abbieogero@gmail.com>
2024-09-11 21:00:45 +0300

    first commit

---
+++ 01_hello.js
@@ -0,0 +1,28 @@
+console.log("Hello world");
+
+//Printing out Hello World in Javascript
+//In the terminal you can type node test.js  in the terminal and enter
+
+let n = 133;
+//let is only used when assigning the number for the very first time
+n = 25;
+// you just use the variable to reassign it, you don't use let again as it was already declared
+console.log(n);
+
+let score;
+//const score;
+//when using the const keyword make sure you assign a value
+
+let id;
+//using const is the same as declaring but ensuring it does not change
+const id_num= 2000;
+console.log(id_num);
+
+
+//Data types in Javascript
+//find datatypes and type name
+let  name = "Musa";
+console.log(typeof name);
+
+//Concactenation
+
